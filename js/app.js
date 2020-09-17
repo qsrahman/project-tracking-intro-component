ff54336c913e36df4b910e2063570cf4ec264e2c
Qazi Sami ur Rahman <qsrahman@hotmail.com>
2020-09-17 18:51:48 +0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const hamburger = document.querySelector('#hamburger')
+const menu = document.querySelector('.menu')
+
+hamburger.addEventListener('click', e => {
+  e.currentTarget.classList.toggle('show')
+  menu.classList.toggle('show')
+})
